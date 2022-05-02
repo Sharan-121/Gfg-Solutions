a3e377299069b9f292db467da753d00ba9853d86
Sharan S S <85382165+Sharan-121@users.noreply.github.com>
2022-05-02 20:36:07 +0530

    finding middle element in linear time

---
+++ Stacks and Queues/findMiddle.java
@@ -0,0 +1,81 @@
+class DDL{
+    class DDLNode{
+        DDLNode prev;
+        int data;
+        DDLNode next;
+        
+        DDLNode(int data){
+            this.data = data;
+        }
+    }
+
+    class MyStack{
+        DDLNode head;
+        DDLNode mid;
+        int count;
+
+        MyStack(){
+            this.count = 0;
+        }
+        void push(int new_data){
+            DDLNode newNode = new DDLNode(new_data);
+            newNode.prev =null;
+            newNode.next = this.head;
+            this.count = this.count + 1;
+
+            if(this.count == 1){
+                this.mid = newNode;
+            }
+            else{
+                this.head.prev = newNode;
+                if(count % 2 != 0){
+                    this.mid = this.mid.prev;
+                }
+            }
+            this.head = newNode;
+        }
+
+        int pop(){
+            if(this.count == 0){
+                return - 1;
+            }
+            DDLNode temp = this.head;
+            int item = temp.data;
+
+            this.head = this.head.next;
+            if(this.head != null){
+                this.head.prev = null;
+            }
+            this.count -=1;
+
+            if(this.count % 2 == 0){
+                this.mid = this.mid.next;
+            }
+            return item;
+        }
+        void printMiddle(){
+            if(this.count == 0){
+                return;
+            }
+            System.out.println(this.mid.data);
+        }
+    }
+}
+
+public class findMiddle {
+    public static void main(String[] args) {
+        DDL obj = new DDL();
+        DDL.MyStack obj1 = obj.new MyStack();
+        obj1.push(1);
+        obj1.push(2);
+        obj1.push(3);
+        obj1.push(4);
+        obj1.push(5);
+        // obj1.push(6);
+        System.out.println(  obj1.pop());
+      
+        obj1.printMiddle();
+
+        
+    }
+}
